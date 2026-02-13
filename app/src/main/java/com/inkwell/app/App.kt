1e4b2f66a7b10a29a52545c2cfd4ca9910b2f680
DH <perhapsbatman@gmail.com>
2026-02-13 16:36:09 -0500

    Opt in to ExperimentalMaterial3Api for top app bar usage

--- app/src/main/java/com/inkwell/app/App.kt
+++ app/src/main/java/com/inkwell/app/App.kt
@@ -14,7 +14,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.material3.ExperimentalMaterial3Api
 
+
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun App() {
     MaterialTheme {
